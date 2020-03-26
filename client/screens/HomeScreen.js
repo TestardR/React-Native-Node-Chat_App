57d54cd43742cde9de724a102bce52f52b8b91c1
unknown <rmtestard@gmail.com>
2020-03-26 21:02:04 +0100

    I have implemented basic gitedChat to our app

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
 import io from 'socket.io-client';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const HomeScreen = () => {
   const [messageToSend, setMessageToSend] = useState('');
@@ -9,43 +10,29 @@ const HomeScreen = () => {
 
   useEffect(() => {
     socket.current = io('http://192.168.1.86:3001');
-    socket.current.on('message', message => {
-      setReceivedMessages(prevState => [...prevState, message]);
+    socket.current.on("message", message => {
+      setReceivedMessages(prevState => GiftedChat.append(prevState, message));
     });
   }, []);
 
-  const handleMessage = () => {
-    socket.current.emit('message', messageToSend);
-    setMessageToSend('');
+  const onSend = messages => {
+    console.log(messages);
+    socket.current.emit("message", messages[0].text);
   };
 
-  const textOfReceivedMessages = receivedMessages.map(message => {
-    return <Text>{message}</Text>;
-  });
 
   return (
-    <View style={styles.container}>
-      {textOfReceivedMessages}
-      <Text>Hello React Native</Text>
-      <TextInput
-        placeholder="Enter chat message"
-        value={messageToSend}
-        onChangeText={text => {
-          setMessageToSend(text);
+    <View style={{ flex: 1 }}>
+      <GiftedChat
+        messages={receivedMessages}
+        onSend={msg => onSend(msg)}
+        user={{
+          _id: 1
         }}
-        onSubmitEditing={handleMessage}
       />
+      {Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
 
 export default HomeScreen;
