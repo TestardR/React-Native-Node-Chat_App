1e6c687f28264daa5627a2187dc8a2ac87b88357
unknown <rmtestard@gmail.com>
2020-03-27 21:37:30 +0100

    I have removed UI fake messages

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -13,33 +13,11 @@ const HomeScreen = () => {
     socket.current.on('message', message => {
       setReceivedMessages(prevState => GiftedChat.append(prevState, message));
     });
-    setReceivedMessages([
-      {
-        _id: 2,
-        text: 'Hey Buddy, what ya doing ?',
-        createdAt: new Date(),
-        user: {
-          _id: 1,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      },
-      {
-        _id: 1,
-        text: 'Romain, whats up with you lately ? :)',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      }
-    ]);
   }, []);
 
   const onSend = message => {
     console.log(message);
-    socket.current.emit('message', message);
+    socket.current.emit('message', message[0].text);
   };
 
   return (
