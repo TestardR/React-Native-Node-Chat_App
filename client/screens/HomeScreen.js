391af2f5b69c56bc4202fa63eee17881dafe49bb
unknown <rmtestard@gmail.com>
2020-03-27 21:34:07 +0100

    I have added bogus messages to see UI

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -10,16 +10,37 @@ const HomeScreen = () => {
 
   useEffect(() => {
     socket.current = io('http://192.168.1.86:3001');
-    socket.current.on("message", message => {
+    socket.current.on('message', message => {
       setReceivedMessages(prevState => GiftedChat.append(prevState, message));
     });
+    setReceivedMessages([
+      {
+        _id: 2,
+        text: 'Hey Buddy, what ya doing ?',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      },
+      {
+        _id: 1,
+        text: 'Romain, whats up with you lately ? :)',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      }
+    ]);
   }, []);
 
-  const onSend = messages => {
-    console.log(messages);
-    socket.current.emit("message", messages[0].text);
+  const onSend = message => {
+    console.log(message);
+    socket.current.emit('message', message);
   };
-
 
   return (
     <View style={{ flex: 1 }}>
