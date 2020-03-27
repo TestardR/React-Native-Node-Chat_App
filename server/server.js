4db057c4e6f22278b043a114ebe79d1b61bf9f38
unknown <rmtestard@gmail.com>
2020-03-27 21:37:02 +0100

    I have added a test message in the backend for future development

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const Server = require('socket.io');
 const io = new Server();
 
-io.on('connection', (socket) => {
-  console.log('a user connected!');
-  socket.on("message", message => {
-      console.log(message)
-      io.emit("message", message)
-  })
-});
+/* const message = {
+  _id: 1,
+  text: 'Romain, whats up with you lately ? :)',
+  createdAt: new Date(),
+  user: {
+    _id: 2,
+    name: 'React Native',
+    avatar: 'https://placeimg.com/140/140/any'
+  }
+}; */
 
+io.on('connection', socket => {
+  console.log('a user connected!');
+  socket.on('message', message => {
+    console.log(message);
+    io.emit('message', message);
+  });
+});
 
 io.listen(3001);
