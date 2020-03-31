7caecbe60c499ada4488cb2d20920bd97e146506
unknown <rmtestard@gmail.com>
2020-03-31 20:57:48 +0200

    I havation v5

--- client/App.js
+++ client/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import JoinScreen from './screens/JoinScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Join"
+        screenOptions={{
+          headerTintColor: '#00E2AF',
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Chat forever' }}
+        />
+        <Stack.Screen
+          name="Join"
+          component={JoinScreen}
+          options={{ title: 'Chat forever' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
